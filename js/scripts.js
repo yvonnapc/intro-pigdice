a87c469c991ab664b5e0de43c1356cfad78d7247
Sean Peerenboom and Yvonna Contreras <seanmp42@gmail.com>
2016-01-27 10:01:55 -0800

    Add counter variable

--- js/scripts.js
+++ js/scripts.js
@@ -2,78 +2,26 @@ $(document).ready(function() {
   $("form#question1").submit(function(event) {
   var random = Math.floor((Math.random()*6) + 1);
 
-  if (random === 1) {
-    $("#jb").show();
+  var appendText = [];
+
+  for (var i = 0; i < counter++; i+=random) {
+      appendText.push(random); //This adds each thing we want to append to the array in order.
   }
-  else {
-  $("#jb").hide();
+  console.log(appendText);
+
+  var new_array = appendText.concat(); //Copy initial array
+
+  for (var i = 1; i < appendText.length; i++) {
+    new_array[i] = new_array[i-1] + appendText[i];
   }
-  if (random === 2){
-    $("#worf").show();
-  }
-  else {
-  $("#worf").hide();
-  }
-  if (random === 3) {
-  $("#frodo").show();
-  } else {
-    $("#frodo").hide();
-  }
-  if (random === 4) {
-    $("#got").show();
-  } else {
-    $("#got").hide();
-  }
-  if (random === 5) {
-  $("#beth").show();
-    } else {
-  $("#beth").hide();
-  }
-  if (random === 5) {
-  $("#beth").show();
-    } else {
-  $("#beth").hide();
-  }
+
+  console.log(new_array);
+  appendText = appendText.join(" ");
   event.preventDefault();
   });
   });
 
-  $("form#question2").submit(function(event) {
-  var random = Math.floor((Math.random()*6) + 1);
-
-  if (random === 1) {
-    $("#jb").show();
-  }
-  else {
-  $("#jb").hide();
-  }
-  if (random === 2){
-    $("#worf").show();
-  }
-  else {
-  $("#worf").hide();
-  }
-  if (random === 3) {
-  $("#frodo").show();
-  } else {
-    $("#frodo").hide();
-  }
-  if (random === 4) {
-    $("#got").show();
-  } else {
-    $("#got").hide();
-  }
-  if (random === 5) {
-  $("#beth").show();
-    } else {
-  $("#beth").hide();
-  }
-  if (random === 5) {
-  $("#beth").show();
-    } else {
-  $("#beth").hide();
-  }
-  event.preventDefault();
-  });
-  });
-});
+  // event.preventDefault();
+  // });
+  // });
+// });
