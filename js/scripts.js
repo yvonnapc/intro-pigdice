ab7423b00a892e07638d610fc9ea55f5c32b0efc
Sean Peerenboom and Yvonna Contreras <seanmp42@gmail.com>
2016-01-27 14:32:05 -0800

    Add player function, arrays in the works

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
+function Player {
+  this.Player = Player;
+  this.turnscore = turnscore;
+  this.totalscore = totalscore;
+}
+
 $(document).ready(function() {
 $("form#question").submit(function(event) {
 
 var random = Math.floor((Math.random()*6) + 1);
-console.log(random);
-$("#sth").append(<li );
+var superarray = [];
+
+superarray.push(random);
+ // return superarray;
+
+console.log(superarray);
+// $("#sth").append(<li );
 
 event.preventDefault();
 });
