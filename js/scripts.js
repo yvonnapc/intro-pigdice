e9624b38c6959fb8969d27b07f63bf28af00caf2
Sean Peerenboom and Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-01-27 13:02:58 -0800

    Continue editing scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,10 @@
 $(document).ready(function() {
-  $("form#question1").submit(function(event) {
-  var random = Math.floor((Math.random()*6) + 1);
+$("form#question").submit(function(event) {
 
-  var appendText = [];
+var random = Math.floor((Math.random()*6) + 1);
+console.log(random);
+$("#sth").append(<li );
 
-  for (var i = 0; i < 1; i+=random) {
-      appendText.push(random); //This adds each thing we want to append to the array in order.
-  }
-
-  var new_array = appendText.concat(); //Copy initial array
-
-  for (var i = 1; i < appendText.length; i++) {
-    new_array[i] = new_array[i] + appendText[i];
-  }
-
-  console.log(new_array);
-  appendText = appendText.join(" ");
-  event.preventDefault();
-  });
-  // $("button#william").click(function(event) {
-  //   var danger = counter++;
-  });
-  // });
-
-  // event.preventDefault();
-  // });
-  // });
-// });
+event.preventDefault();
+});
+});
