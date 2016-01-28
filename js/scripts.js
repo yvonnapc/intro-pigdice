00055db608d0d18439d4dafd7202d2923cc31154
Sean Peerenboom and Yvonna Contreras <seanmp42@gmail.com>
2016-01-27 16:28:59 -0800

    Rough draft still in the works

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,83 @@
-function Player {
-  this.Player = Player;
-  this.turnscore = turnscore;
-  this.totalscore = totalscore;
+// function rollin(player, dicemaster){
+//       this.player = player;
+//       this.dicemaster =[];
+//   }
+//   Order.prototype.totalPies = function(){
+//     var rolling = 0;
+//     this.dicemaster.forEach(function(dicers){
+//       rolling = rolling + Pizza.numberPizzas;
+//     });
+//     return rolling;
+//   }
+// function testing(){
+
+var currentPlayer = player1;
+
+function player(name){
+  this.name = name;
+  this.rolls = [];
+}
+
+
+
+Player.prototype.takeTurn = function(){
+  var random = Math.floor((Math.random()*6) + 1);
+  this.rolls.push(random);
+}
+
+Player.prototype.endTurn = function(){
+
+
+var player1 = new Player("Courtney");
+
+player1.takeTurn()
+this.rolls = [2];
+player1.takeTurn()
+this.rolls = [2, 4, ]
+
+
+
+var appendText = [];
+for (var i = 0; i < 1; i++) {
+    appendText.push(random); //This adds each thing we want to append to the array in order.
+  }
+}
+var new_array = appendText.concat(); //Copy initial array
+
+for (var i = 1; i < appendText.length; i+random) {
+  new_array[i] = new_array[i-1] + appendText[i];
+}
 }
 
 $(document).ready(function() {
 $("form#question").submit(function(event) {
+  function
+  var random = Math.floor((Math.random()*6) + 1);
+  var appendText = [];
+  for (var i = 0; i < 1; i++) {
+      appendText.push(random); //This adds each thing we want to append to the array in order.
+  }
 
-var random = Math.floor((Math.random()*6) + 1);
-var superarray = [];
+  var new_array = appendText.concat(); //Copy initial array
 
-superarray.push(random);
- // return superarray;
+  for (var i = 1; i < appendText.length; i+random) {
+    new_array[i] = new_array[i-1] + appendText[i];
+  }
 
-console.log(superarray);
-// $("#sth").append(<li );
-
+  if (random === 1) {
+    $("#sth").append("Next Player");
+  } else {
+    var dnd= random;
+  console.log(dnd);
+  }
+// var superarray = [];
+//
+// superarray.push(random);
+//  // return superarray;
+//
+// console.log(superarray);
+// // $("#sth").append(<li );
+//
 event.preventDefault();
 });
 });
